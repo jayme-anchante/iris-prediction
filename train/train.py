a48eb5ffde9fac917f48fb661bb0233568e9cbaf
Jayme Anchante <jayme.anchante@disroot.org>
2019-09-24 11:30:26 -0300

    renamed porter to train file

--- train/train.py
+++ train/train.py
@@ -1,20 +1,18 @@
-import numpy as np
-import xgboost as xgb
+from sklearn.datasets import load_iris
+from sklearn.tree import tree
+from sklearn_porter import Porter
+import pickle
 
-data = np.genfromtxt("iris.csv", dtype="f8,f8,f8,f8,U10", delimiter=",", names=True)
+# Load data and train the classifier:
+samples = load_iris()
+X, y = samples.data, samples.target
+clf = tree.DecisionTreeClassifier()
+clf.fit(X, y)
 
-rng = np.random.RandomState()
+# Export:
+porter = Porter(clf, language='js')
+output = porter.export(embed_data=True)
+print(output)
 
-valid_index = rng.choice([i for i in range(len(data))], 30, False)
-valid_index = [i in valid_index for i in range(len(data))]
-train_index = [i == False for i in valid_index]
-
-train = np.concatenate([data[name][train_index].reshape(-1, 1) for name in
-                        data.dtype.names], axis=1)
-valid = np.concatenate([data[name][valid_index].reshape(-1, 1) for name in
-                        data.dtype.names], axis=1)
-
-dtrain = xgb.DMatrix(train[:, :4], label=train[:, 4])]])
-dvalid = xgb.DMatrix(valid[:, :4], label=valid[:, 4])]])
-
-# https://www.kdnuggets.com/2017/03/simple-xgboost-tutorial-iris-dataset.html
+with open("clf.js", "w") as file:
+    file.write(output)
