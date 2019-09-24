4c43c6e001bf77f2e619ed3ce68ed06c2fc6ee2d
Jayme Anchante <jayme.anchante@disroot.org>
2019-09-24 12:13:48 -0300

    updated train.py * removed unnecessary imports * splitting the data between train and test * checking the test accuracy * using a random forest as classifier * removed print statement

--- train/train.py
+++ train/train.py
@@ -1,18 +1,40 @@
 from sklearn.datasets import load_iris
-from sklearn.tree import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn_porter import Porter
-import pickle
 
 # Load data and train the classifier:
 samples = load_iris()
 X, y = samples.data, samples.target
-clf = tree.DecisionTreeClassifier()
-clf.fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    shuffle=True,
+    stratify=y
+)
+clf = RandomForestClassifier(
+    n_estimators=10,
+    min_samples_split=3,
+    min_samples_leaf=3,
+    max_depth=5,
+    max_features=2,
+    oob_score=True,
+    random_state=42
+)
+clf.fit(X_train, y_train)
+
+# Checking the accuracy in the holdout set:
+y_pred = clf.predict(X_test)
+accuracy = round(accuracy_score(y_test, y_pred) * 100, 2)
+
+print("\n\nAccuracy in the holdout set of 30 samples:", str(accuracy), "%\n\n")
+
 
 # Export:
-porter = Porter(clf, language='js')
+porter = Porter(clf, language="js")
 output = porter.export(embed_data=True)
-print(output)
-
 with open("clf.js", "w") as file:
     file.write(output)
